3975b4534eb8abe91bacc9461d58f836972d96bb
duchv90 <hvduc.90@gmail.com>
2019-04-15 14:01:49 +0700

    INIT2

---
+++ src/components/headers/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../../logo.png';
+
+const Header = () => (
+  <div className="header__inner">
+    <img className="logo" src="{logo}" alt="Logo" />
+  </div>
+);
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootSaga from './rootSaga';
+import rootReducer from './rootReducer';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
